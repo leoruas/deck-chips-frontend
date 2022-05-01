f59dcfcb5ec0530398991d735e5030b9e490cfe3
leoruas <leonardo.ruas@going2.com.br>
2022-05-01 17:01:41 -0300

    setup i18n for translation

--- src/features/auth/Login/index.tsx
+++ src/features/auth/Login/index.tsx
@@ -2,15 +2,18 @@ import { Box } from '@shared/components/layout/Box';
 import { SafeAreaBox } from '@shared/components/layout/SafeAreaBox';
 import Text from '@shared/components/Text';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { StatusBar } from 'react-native';
 
 export default function LoginScreen() {
+  const { t } = useTranslation('login');
+
   return (
     <SafeAreaBox flex={1} justifyContent="center">
       <StatusBar translucent backgroundColor={'transparent'} />
       <Box>
         <Text adjustsFontSizeToFit textAlign="center" fontSize={20}>
-          Tela de Login
+          {t('title')}
         </Text>
       </Box>
     </SafeAreaBox>
---
+++ src/i18n/pt-br.js
@@ -0,0 +1,5 @@
+export default {
+  login: {
+    title: 'Tela de Login',
+  },
+};
